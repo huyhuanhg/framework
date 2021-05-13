4356f901bd7fd43516b895d5e0b39463e08f5364
Hoang Huy Hung <hung.hoang@paradox.ai>
2021-05-13 16:50:30 +0700

    clear code

--- config/database.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'host' => 'localhost',
-    'user' => 'root',
-    'password' => 'Mysql@huan95',
-    'dbName' => 'english'
-];
--- public/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-define("__DIR_PUBLIC__", __DIR__);
-require_once  dirname(__DIR_PUBLIC__)."/bootstrap/bootstrap.php";
-require_once (dirname(__DIR_PUBLIC__).'/app/core/App.php');
-App::run();
