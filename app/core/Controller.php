383661cafa0f41bf377c1a616b961da86387f751
huyhuanhg <huyhuanhg@gmail.com>
2021-05-17 19:55:01 +0700

    temporarily completed

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -7,9 +7,15 @@ use \App;
 class Controller
 {
     private $layout = null;
-
+    public $request, $response;
     public function __construct()
     {
+        //thuc thi middleware
+            Middleware::runBeforeMiddleware();
+
+        new Helper('app/helpers');
+        $this->request = new Request();
+        $this->response = new Response();
         if (isset(Registry::getIntance()->web['layout'])) {
             $this->layout = Registry::getIntance()->web['layout'];
         } else {
@@ -27,17 +33,14 @@ class Controller
      * @param bool $isEnd
      * @param int $resPonseCode
      */
-    public function redirect($url, $isEnd = true, $resPonseCode = 302)
+    public function redirect($uri, $isEnd = true, $resPonseCode = 302)
     {
-        header('Location:' . $url, true, $resPonseCode);
-        if ($isEnd) {
-            die();
-        }
+        $this->response->redirect($uri);
     }
 
     public function render($viewFile, $data = null, $layout = null)
     {
-        if (isset($layout)){
+        if (isset($layout)) {
             $this->setLayout($layout);
         }
         $viewContent = $this->getViewContent($viewFile, $data);
@@ -49,9 +52,35 @@ class Controller
         }
     }
 
+    /*
+        public function renderMultiContent($listViewContentData, $layout = null)
+        {
+            if (isset($layout)) {
+                $this->setLayout($layout);
+            }
+            $content = [];
+            foreach ($listViewContentData as $filePath => $data) {
+                $fileArr = explode('/', $filePath);
+                $file = end($fileArr);
+                $content[$file] = $this->getViewContent($filePath, $data);
+                die("loi o function getViewContent");
+            }
+            echo "<pre>";
+
+            extract($content, EXTR_PREFIX_SAME, 'data');
+
+            if ($this->layout !== null) {
+                $layoutPath = __DIR_ROOT__ . '/app/views/' . $this->layout . '.php';
+                if (file_exists($layoutPath)) {
+                    require_once($layoutPath);
+                }
+            }
+        }
+    */
     public function getViewContent($viewFile, $data = null)
     {
-        $viewFolder = strtolower(str_replace('Controller', '', Registry::getIntance()->controller));
+        $ctlDir = strtolower(Session::flash('controller'));
+        $viewFolder = strtolower(str_replace('Controller', '', $ctlDir));
         $viewPath = __DIR_ROOT__ . '/app/views/' . $viewFolder . '/' . $viewFile . '.php';
         if (is_array($data)) {
             extract($data, EXTR_PREFIX_SAME, 'data');
@@ -60,12 +89,12 @@ class Controller
         }
         if (file_exists($viewPath)) {
             ob_start();
-            require($viewPath);
+            require_once($viewPath);
             return ob_get_clean();
         }
     }
 
-    public function renderPartial($view, $data = null)
+    public static function view($view, $data = null)
     {
         $viewPath = __DIR_ROOT__ . '/app/views/' . $view . '.php';
         if (is_array($data)) {
@@ -74,7 +103,13 @@ class Controller
             $data = $data;
         }
         if (file_exists($viewPath)) {
-            require($viewPath);
+            require_once($viewPath);
         }
+    }
+
+    public function model($model)
+    {
+        require_once __DIR_ROOT__ . "/app/models/" . $model . ".php";
+        return new $model();
     }
 }
--- public/index.php
+++ public/index.php
@@ -1,5 +1,5 @@
 <?php
-
+session_start();
 define("__DIR_PUBLIC__", __DIR__);
 require_once  dirname(__DIR_PUBLIC__)."/bootstrap/bootstrap.php";
 require_once (dirname(__DIR_PUBLIC__).'/app/core/App.php');
